794a439bda015befdab334486dc1eec1077c2fc7
Anton Astremskyi <lukcytony@Antons-MBP.home>
2020-03-18 02:49:22 -0400

    Header/Navbar/Dashboard components created

---
+++ src/components/Dashboard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return(
+        <main>
+        <div><img src="https://apexcharts.com/wp-content/uploads/2018/05/area-chart-spline.svg"></img></div>
+        <div>Avatar + information</div>
+        <div>
+          My posts
+          <div>New Post Form</div>
+          <div>Post 1</div>
+          <div>Post 2</div>
+          <div>Post 3</div>
+        </div>
+      </main>
+    )
+}
+
+export default Dashboard;
---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Nav = () => {
+    return(
+        <nav>
+        <div><a href="#">Dashboard</a></div>
+        <div><a href="#">Expenses</a></div>
+        <div><a href="#">Subscriptions</a></div>
+        <div><a href="#">Emploees</a></div>
+        <div><a href="#">Payments</a></div>
+        <div><a href="#">Requests</a></div>
+        <div><a href="#">Cards</a></div>
+        <div><a href="#">Settings</a></div>
+      </nav>
+    )
+}
+
+export default Nav;
