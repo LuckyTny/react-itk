8ba5f3938094532862da5e37c9129f22bcda86af
Athony A <astremsky@gmail.com>
2020-04-20 01:35:13 -0400

    Class component for Users

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -8,6 +8,7 @@ const Nav = () => {
         <nav>
         <div className={style.item}><NavLink to="/profile" activeClassName={style.active}>Profile</NavLink></div>
         <div className={style.item}><NavLink to="/dialogs" activeClassName={style.active}>Messages</NavLink></div>
+        <div className={style.item}><NavLink to="/users" activeClassName={style.active}>Users</NavLink></div>
         <div className={style.item}><NavLink to="news" activeClassName={style.active}>News</NavLink></div>
         <div className={style.item}><NavLink to="music" activeClassName={style.active}>Music</NavLink></div>
         <div className={style.item}><NavLink to="settings" activeClassName={style.active}>Settings</NavLink></div>
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,31 +2,35 @@ import React from 'react'
 import styles from './Users.module.css'
 import * as axios from 'axios'
 
-let Users = (props) => {
-    if (props.users.length === 0) {
-        debugger;
+import avatarHolder from './../../assets/images/Portrait_Placeholder.png'
+
+class Users extends React.Component {
+  
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then( response => {
-            console.log(response)
-            props.setUsers(response.data.items)
+        
+            this.props.setUsers(response.data.items)
         })
     }
-
-    return <div> <h2> USER COMPONENT WORKS</h2>
-            {/* {{props.users.map( u => <div key={u.id}>
-                <img className={styles.photo} src={u.photoUrl} alt=""/>
+  
+    render() {
+        return <div>
+            {this.props.users.map( u => <div key={u.id}>
+                <img className={styles.photo} src={u.photos.small != null ? u.photos.small : avatarHolder } alt=""/>
                 <div>
                 { u.followed 
-                    ? <button onClick={() => { props.follow(u.id)}} >Follow</button> 
-                    : <button onClick={() => { props.unfollow(u.id)}} >Unfollow</button> }
+                    ? <button onClick={() => { this.props.follow(u.id)}} >Follow</button> 
+                    : <button onClick={() => { this.props.unfollow(u.id)}} >Unfollow</button> }
                 </div>
-                <div>{u.fullName}</div>
+                <div>{u.name}</div>
                 <div>{u.status}</div>
-                <div>{u.location.city}</div>
-                <div>{u.location.country}</div>
+                <div>{'u.location.city'}</div>
+                <div>{'u.location.country'}</div>
                 
             </div>)
-        }} */}
+        }
     </div>
+    }
 }
 
 export default Users
